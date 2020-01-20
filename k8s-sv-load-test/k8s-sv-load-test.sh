56363a8efc0daa38ca8e636ef6f85b04f302b83a
Juan Medina <jamedina@gmail.com>
2020-01-20 22:26:55 +0000

    working on heap

--- k8s-sv-load-test/k8s-sv-load-test.sh
+++ k8s-sv-load-test/k8s-sv-load-test.sh
@@ -21,17 +21,6 @@ LOAD_DIR="$(
   cd "$(dirname "$0")"
   pwd -P
 )"
-REPORT_DIR="$LOAD_DIR/report"
-
-if [ -d "$REPORT_DIR" ]; then
-  echo "deleting report directory"
-  rm -Rf "$REPORT_DIR"
-  echo "report directory deleted"
-fi
-
-echo "creating report directory"
-mkdir "$REPORT_DIR"
-echo "report directory created"
 
 SERVICE=$1
 TEST_URL=$2
@@ -41,19 +30,4 @@ NUM_USERS=$3
 DURATION=$4
 RAMP_UP=$5
 
-echo "launching a load test on http://$SERVICE_CLUSTER_IP:$SERVICE_PORT$TEST_URL with $NUM_USERS "\
-  "user(s) during $DURATION seconds, ramping up during $RAMP_UP seconds"
-
-HEAP="-Xms1g -Xmx1g -XX:MaxMetaspaceSize=256m"
-jmeter -n -t "$LOAD_DIR/load_test.jmx" -l "$REPORT_DIR/run.jtl" -e -o "$REPORT_DIR" -j "$LOAD_DIR/jmeter.log" \
-  -JSERVICE_CLUSTER_IP="$SERVICE_CLUSTER_IP" \
-  -JSERVICE_PORT="$SERVICE_PORT" \
-  -JNUM_USERS="$NUM_USERS" \
-  -JDURATION="$DURATION" \
-  -JRAMP_UP="$RAMP_UP" \
-  -JTEST_URL="$TEST_URL"
-
-echo "load test done"
-
-WEB_REPORT_PATH="file:///$REPORT_DIR/index.html"
-echo "report available on $WEB_REPORT_PATH"
+$LOAD_DIR/load_test.sh $SERVICE_CLUSTER_IP $SERVICE_PORT $TEST_URL $NUM_USERS $DURATION $RAMP_UP
